b34cf1df612e1d34a1b4a5b64301535070f215a8
Mihai Sturza <mihai.sturza@lateral-inc.com>
2019-04-30 08:48:38 +0300

    first commit from mac

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,13 @@
 import * as mongoose from 'mongoose';
 import { ObjectId } from 'bson';
 
-mongoose.connect('mongodb://localhost/mongo-exercises', {useNewUrlParser: true})
+mongoose.connect('mongodb://localhost/mongo-exercises', { useNewUrlParser: true })
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error(err))
 
 
 const schema = new mongoose.Schema({
-  tags: {
-    type: Array,
-    validate: {
-      isAsync: true,
-      validator: function(v, callback) {
-        // do some async work
-        setTimeout(() => {
-          const result = v && v.length > 0;
-          callback(result); 
-        }, 4000);
-      }
-    }
-  },
+  tags: { type: Array },
   date: { type: Date, default: Date.now },
   name: {
     type: String,
@@ -39,18 +27,18 @@ const schema = new mongoose.Schema({
     type: Number,
     min: 10,
     max: 200,
-    required: function() { return this.isPublished; }
+    required: function () { return this.isPublished; }
   }
 })
 
 const Course = mongoose.model('course', schema);
 
-async function createCourse (schema) {
+async function createCourse(schema) {
   const newCourse = new Course(schema);
   try {
     const result = await newCourse.save();
     console.log(result);
-  } catch(err) {
+  } catch (err) {
     console.log('Name: ', err.name);
     console.log('Message: ', err.message);
   }
@@ -58,10 +46,10 @@ async function createCourse (schema) {
 
 async function getCourses() {
   return await Course
-  .find({isPublished: true})
-  .or([ {price: { $gte: 100 }}, {name: /.*by.*/i } ])
-  .sort({price: -1})
-  .select('name author price')
+    .find({ isPublished: true })
+    .or([{ price: { $gte: 100 } }, { name: /.*by.*/i }])
+    .sort({ price: -1 })
+    .select('name author price')
 }
 
 async function run() {
@@ -70,13 +58,13 @@ async function run() {
 }
 
 async function updateCourse(query, update) {
-  const course = await Course.findOneAndUpdate(query, update, {new: true})
+  const course = await Course.findOneAndUpdate(query, update, { new: true })
   console.log(course);
 }
 
 async function removeCourse(id) {
- const result = await Course.deleteOne({ _id: id });
- console.log(result);
+  const result = await Course.deleteOne({ _id: id });
+  console.log(result);
 }
 
 
@@ -93,17 +81,24 @@ async function findCourse(condition) {
 }
 
 async function deleteItem() {
-  const theOne = await findCourse({ author: 'Jack', name: 'Mimi'})
+  const theOne = await findCourse({ author: 'Jack', name: 'Mimi' })
   removeCourse(theOne._id);
 }
 
 // deleteItem();
 
-createCourse({
-    tags: [],
-    name: 'Enu Maia',
-    category: 'web',
-    author: 'SundaQ',
-    isPublished: true,
-    price: 25
-});
+// createCourse({
+//   tags: [],
+//   name: 'Enumiya Inca',
+//   category: 'web',
+//   author: 'Sundari Mam',
+//   isPublished: true,
+//   price: 35
+// });
+
+async function showCourses() {
+  const result = await Course.find();
+  console.log(result);
+}
+
+showCourses();
